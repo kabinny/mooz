77fc52a8862dfd4fe3eef2b9275017612e153580
kabinny <kabinny@gmail.com>
2022-10-14 19:01:52 +0900

    User Count

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from "http"
 import SocketIO from "socket.io"
 // import WebSocket from "ws"
 import express from "express"
+import { count } from 'console'
 
 const app = express()
 
@@ -27,6 +28,10 @@ function publicRooms() {
   return publicRooms
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon"
   socket.onAny((event) => {
@@ -36,11 +41,13 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName)
     done()
-    socket.to(roomName).emit("welcome", socket.nickname)
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName))
     wsServer.sockets.emit("room_change", publicRooms())
   })
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname))
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+    })
   })
   socket.on("disconnect", () => {
     wsServer.sockets.emit("room_change", publicRooms())
